e87c782d096295b8d140184bd6578d8058dfa7d6
jkamz <jkamz6432@gmail.com>
2019-01-21 13:46:29 +0300

    [feature#163356831] add signup feature

--- models.py
+++ models.py
@@ -1,5 +1,28 @@
-""" All the data will be stored here """"
+""" All the data will be stored here """
 
 users = []
 
 comments = []
+
+
+class UserModel():
+    """
+    user model
+    """
+
+    def signup(self, username, email, password, confirm_password, role):
+        """
+        user signup model
+        """
+
+        user = {
+            "id": len(users) + 1,
+            "username": username,
+            "email": email,
+            "password": password,
+            "confirm_password": confirm_password,
+            "role": role
+        }
+
+        users.append(user)
+        return user
--- user.py
+++ user.py
@@ -1,8 +1,22 @@
+from models import UserModel
+
+
 class User:
-    pass
+
+    def signUp(self):
+
+        username = input("please enter your username")
+        email = input("please enter your email")
+        password = input("please enter your password")
+        confirm_password = input("please confirm password")
+        role = input("enter role")
+
+        UserModel.signup(username, email, password, confirm_password, role)
+
 
 class Moderator(User):
     pass
 
-class Admin(Moderator)
+
+class Admin(Moderator):
     pass
