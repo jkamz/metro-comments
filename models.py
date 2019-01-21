711ab591489e2ff88c690298190a489fde1b5a1e
jkamz <jkamz6432@gmail.com>
2019-01-21 15:39:53 +0300

    [ft#163356856] create comment

--- models.py
+++ models.py
@@ -3,3 +3,37 @@
 users = []
 
 comments = []
+
+
+class UserModel():
+    """
+    user model
+    """
+
+    def signup(self, username, email, password, role):
+        """
+        user signup model
+        """
+
+        user = {
+            "id": len(users) + 1,
+            "username": username,
+            "email": email,
+            "password": password,
+            "role": role
+        }
+
+        users.append(user)
+        return user
+
+
+class CommentsModel():
+
+    def create_comment(self, author, comment):
+        comment = {
+            "author": author,
+            "comment": comment
+        }
+
+        comments.append(comment)
+        return comment
